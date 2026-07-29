12e82f42c3192a99b96bd96bc7fd484196124ed7
cinnamahi <mahikad06@gmail.com>
2026-07-29 18:00:24 -0400

    part 4

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 //Dragging Logic
 
-dragElement(document.getElementById("window"));
-
 function dragElement(element) {
     var initialX = 0;
     var initialY = 0;
@@ -21,14 +19,14 @@ function dragElement(element) {
         initialX = e.clientX;
         initialY = e.clientY;
         document.onmouseup = stopDragging;
-        document.onmousemove = dragElement;
+        document.onmousemove = moveElement;
     }
 
-    function dragElement(e) {
+    function moveElement(e) {
             e = e || window.event;
         e.preventDefault();
         currentX = initialX - e.clientX;
-        currentY = intialY - e.clientY;
+        currentY = initialY - e.clientY;
         initialX = e.clientX;
         initialY = e.clientY;
         element.style.top = (element.offsetTop - currentY) + "px";
@@ -48,7 +46,11 @@ function updateTime() {
         }
 setInterval(updateTime, 1000);
 
-//Open and Close
+//Drag Apps
+dragElement(document.getElementById("#welcome"));
+dragElement(document.getElementById("#projects"))
+
+// Open and Close Windows
 
 function closeWindow(element) {
     element.style.display = "none"
@@ -56,11 +58,14 @@ function closeWindow(element) {
 
 function openWindow(element) {
     element.style.display = "flex" //try block instead later
+    biggestIndex++; 
+    element.style.zIndex = biggestIndex;
+    taskBar.style.zIndex = biggestIndex + 1;
 }
 
 var welcomeScreen = document.querySelector("#welcome");
 var welcomeScreenClose = document.querySelector("#welcomeclose");
-var welcomeScreenOpen = document.querySelector("#welcomeopen")
+var welcomeScreenOpen = document.querySelector("#welcomeopen");
 
 welcomeScreenClose.addEventListener("click", function() {
     closeWindow(welcomeScreen);
@@ -68,3 +73,64 @@ welcomeScreenClose.addEventListener("click", function() {
 welcomeScreenOpen.addEventListener("click", function() {
     openWindow(welcomeScreen);
 });
+
+var projectsScreen = document.querySelector("#projects");
+var projectsScreenClose = document.querySelector("#projectsclose");
+var projectsScreenOpen = document.querySelector("#projectsopen");
+
+projectsScreenClose.addEventListener("click", function() {
+    closeWindow(projectsScreen);
+});
+projectsScreenOpen.addEventListener("click", function() {
+    openWindow(projectsScreen);
+});
+
+
+//Open Apps
+
+var selectedIcon = undefined;
+
+function selectIcon(element) {
+    element.classList.add("selected");
+    selectedIcon = element;
+}
+
+function deselectIcon(element) {
+    element.classList.remove("selected");
+    selectedIcon = undefined;
+}
+
+function handleIconTap(element) {
+    if (element.classList.contains("selected")) {
+        deselectIcon(element)
+        openWindow(window)
+    } else {
+        selectIcon(element)
+    }
+}
+
+// Rise Up
+
+var biggestIndex = 1;
+var taskBar = document.querySelector("#taskbar");
+
+function addWindowTapHandling(element) {
+    element.addEventListener("mousedown", () =>
+        handleWindowTap(element)
+    )
+}
+
+function handleWindowTap(element) {
+    biggestIndex++;
+    element.style.zIndex = biggestIndex;
+    taskBar.style.zIndex = biggestIndex + 1;
+    deselectIcon(selectedIcon)
+}
+
+//Initialize Window
+
+function createWindow(element) {
+    var screen = document.querySelector("#" + element);
+    addWindowTapHandling(screen);
+    dragElement(screen);
+}
